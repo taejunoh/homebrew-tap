d98a59ee45c70da1b121074a83b648bfb6634f69
Taejun Oh <taejun55@gmail.com>
2026-04-15 17:47:09 -0400

    Update claudebeat cask SHA256 (slider range fix)

--- Casks/claudebeat.rb
+++ Casks/claudebeat.rb
@@ -1,6 +1,6 @@
 cask "claudebeat" do
   version "1.0.0"
-  sha256 "26dc05d7f7211ddb6f084687eb8ee842d4532894c33ea29aee277126bf0b2085"
+  sha256 "38b20974b251a25af3e4d261a2021ef0c952129b57f7b938ebbbee68faa3a6c4"
 
   url "https://github.com/taejunoh/ClaudeBeat/releases/download/v#{version}/ClaudeBeat.zip"
   name "ClaudeBeat"
