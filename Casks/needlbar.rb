60aaa3028d570b826c23ebab9cca67eadf5ca2eb
Taejun Oh <taejun55@gmail.com>
2026-09-16 06:59:57 -0400

    Update Needlbar to v0.3.3

--- Casks/needlbar.rb
+++ Casks/needlbar.rb
@@ -1,14 +1,14 @@
 cask "needlbar" do
-  version "0.3.2"
-  sha256 "3db2e6bb7aadf7eb1bb26de29139b95073a866428d706f4fc08b6314e4b741c3"
+  version "0.3.3"
+  sha256 "79ac52b74972e51eac9c603f62cc6dc8b695a11b333ea92ecbe43e17a7ff7f0f"
 
   url "https://github.com/taejunoh/needlbar/releases/download/v#{version}/Needlbar-macos-arm64.zip"
   name "Needlbar"
   desc "Menu-bar monitor for AI usage and system health"
   homepage "https://github.com/taejunoh/needlbar"
 
-  depends_on macos: :sonoma
   depends_on arch: :arm64
+  depends_on macos: :sonoma
 
   app "Needlbar.app"
 end
