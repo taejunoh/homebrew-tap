585cb012c98cef47efc950bf9b80ba6cea56abdd
Taejun Oh <taejun55@gmail.com>
2026-09-22 21:11:34 -0400

    chore: update Needlbar cask to v0.3.4

--- Casks/needlbar.rb
+++ Casks/needlbar.rb
@@ -1,6 +1,6 @@
 cask "needlbar" do
-  version "0.3.3"
-  sha256 "79ac52b74972e51eac9c603f62cc6dc8b695a11b333ea92ecbe43e17a7ff7f0f"
+  version "0.3.4"
+  sha256 "1d5846bde7c256fb7f5673aec194ba6f29dfea63fb5cb5018a3f4027e49ddf29"
 
   url "https://github.com/taejunoh/needlbar/releases/download/v#{version}/Needlbar-macos-arm64.zip"
   name "Needlbar"
